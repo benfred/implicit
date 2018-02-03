f47e6eca1cf476103ce95fe49eb4bb8bfa0c764b
Ben Frederickson <github@benfrederickson.com>
2018-02-02 16:11:01 -0800

    Add missing files
    Once again missing files from MANIFEST.in (*.cuh preventing the cuda extension from being built).

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import Extension, setup, find_packages
 from cuda_setup import CUDA, build_ext
 
 NAME = 'implicit'
-VERSION = '0.2.10'
+VERSION = '0.2.11'
 
 try:
     from Cython.Build import cythonize
