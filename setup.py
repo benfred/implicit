1d10ae7764724a2660a0e33d7e813a71a307c114
Ben Frederickson <github@benfrederickson.com>
2018-02-07 17:29:51 -0800

    bump version for BPR

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import Extension, setup, find_packages
 from cuda_setup import CUDA, build_ext
 
 NAME = 'implicit'
-VERSION = '0.2.13'
+VERSION = '0.3.0'
 
 try:
     from Cython.Build import cythonize
