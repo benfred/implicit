7156f001ef8a571115acd6d32a40b7fe2b24d44d
Ben Frederickson <github@benfrederickson.com>
2018-05-11 15:55:22 -0700

    only warn once about BLAS config
    If the BLAS env vars are set up to run multiple threads, only warn once.

--- implicit/utils.py
+++ implicit/utils.py
@@ -10,9 +10,18 @@ def nonzeros(m, row):
         yield m.indices[index], m.data[index]
 
 
+_checked_blas_config = False
+
+
 def check_blas_config():
     """ checks to see if using OpenBlas/Intel MKL. If so, warn if the number of threads isn't set
     to 1 (causes severe perf issues when training - can be 10x slower) """
+    # don't warn repeatedly
+    global _checked_blas_config
+    if _checked_blas_config:
+        return
+    _checked_blas_config = True
+
     if np.__config__.get_info('openblas_info') and os.environ.get('OPENBLAS_NUM_THREADS') != '1':
         logging.warning("OpenBLAS detected. Its highly recommend to set the environment variable "
                         "'export OPENBLAS_NUM_THREADS=1' to disable its internal multithreading")
