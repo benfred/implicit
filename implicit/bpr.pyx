benfred/implicit

commit bf905636de28cf05d95c7c891b94677bd5f0647e

Don't run unittests on GPU

implicit/bpr.pyx
@@ -87,12 +87,6 @@ class BayesianPersonalizedRanking(MatrixFactorizationBase):
                  iterations=100, use_gpu=implicit.cuda.HAS_CUDA, num_threads=0):
         super(BayesianPersonalizedRanking, self).__init__()
 
-        if use_gpu and (factors + 1) % 32:
-            padding = 32 - (factors + 1) % 32
-            log.warning("GPU training requires factor size to be a multiple of 32 - 1."
-                        " Increasing factors from %i to %i.", factors, factors + padding)
-            factors += padding
-
         self.factors = factors
         self.learning_rate = learning_rate
         self.iterations = iterations
@@ -181,7 +175,7 @@ class BayesianPersonalizedRanking(MatrixFactorizationBase):
                         "Converting factors to float32")
             self.user_factors = self.user_factors.astype(np.float32)
             self.item_factors = self.item_factors.astype(np.float32)
-
+        
         Ciu = implicit.cuda.CuCOOMatrix(Ciu_host)
         X = implicit.cuda.CuDenseMatrix(self.user_factors)
         Y = implicit.cuda.CuDenseMatrix(self.item_factors)

tests/als_test.py
@@ -14,7 +14,7 @@ from .recommender_base_test import TestRecommenderBaseMixin
 class ALSTest(unittest.TestCase, TestRecommenderBaseMixin):
 
     def _get_model(self):
-        return AlternatingLeastSquares(factors=3, regularization=0)
+        return AlternatingLeastSquares(factors=3, regularization=0, use_gpu=False)
 
     def test_cg_nan(self):
         # test issue with CG code that was causing NaN values in output:
@@ -37,7 +37,8 @@ class ALSTest(unittest.TestCase, TestRecommenderBaseMixin):
                                             regularization=0.01,
                                             dtype=np.float64,
                                             use_native=use_native,
-                                            use_cg=True)
+                                            use_cg=True,
+                                            use_gpu=False)
             model.show_progress = False
             model.fit(counts)
             rows, cols = model.item_factors, model.user_factors

tests/approximate_als_test.py
@@ -13,7 +13,7 @@ try:
 
     class AnnoyALSTest(unittest.TestCase, TestRecommenderBaseMixin):
         def _get_model(self):
-            return AnnoyAlternatingLeastSquares(factors=2, regularization=0)
+            return AnnoyAlternatingLeastSquares(factors=2, regularization=0, use_gpu=False)
 except ImportError:
     pass
 
@@ -23,7 +23,7 @@ try:
     class NMSLibALSTest(unittest.TestCase, TestRecommenderBaseMixin):
         def _get_model(self):
             return NMSLibAlternatingLeastSquares(factors=2, regularization=0,
-                                                 index_params={'post': 2})
+                                                 index_params={'post': 2}, use_gpu=False)
 except ImportError:
     pass
 
@@ -32,7 +32,8 @@ try:
 
     class FaissALSTest(unittest.TestCase, TestRecommenderBaseMixin):
         def _get_model(self):
-            return FaissAlternatingLeastSquares(nlist=1, nprobe=1, factors=2, regularization=0)
+            return FaissAlternatingLeastSquares(nlist=1, nprobe=1, factors=2, regularization=0,
+                                                use_gpu=False)
 except ImportError:
     pass
 

tests/bpr_test.py
@@ -9,7 +9,7 @@ from .recommender_base_test import TestRecommenderBaseMixin
 class BPRTest(unittest.TestCase, TestRecommenderBaseMixin):
 
     def _get_model(self):
-        return BayesianPersonalizedRanking(factors=3, regularization=0)
+        return BayesianPersonalizedRanking(factors=3, regularization=0, use_gpu=False)
 
 
 if HAS_CUDA:

commit fcab11b5e8364969a033cd657581d1c9fed9054f

reinstate warp aligned factors for BPR
This commit: https://github.com/benfred/implicit/commit/bf905636de28cf05d95c7c891b94677bd5f0647e
accidentally removed the check added in
https://github.com/benfred/implicit/commit/be25233a57c58acffa2cfd55f517c7ffb341664c . Reinstate.

implicit/bpr.pyx
@@ -87,6 +87,12 @@ class BayesianPersonalizedRanking(MatrixFactorizationBase):
                  iterations=100, use_gpu=implicit.cuda.HAS_CUDA, num_threads=0):
         super(BayesianPersonalizedRanking, self).__init__()
 
+        if use_gpu and (factors + 1) % 32:
+            padding = 32 - (factors + 1) % 32
+            log.warning("GPU training requires factor size to be a multiple of 32 - 1."
+                        " Increasing factors from %i to %i.", factors, factors + padding)
+            factors += padding
+
         self.factors = factors
         self.learning_rate = learning_rate
         self.iterations = iterations