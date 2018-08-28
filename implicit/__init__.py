6a2fed489d8ee43bef28b98546adde0f10cdb802
Ben Frederickson <github@benfrederickson.com>
2018-08-28 15:53:14 -0700

    add approximate_als import to package __init__

--- implicit/__init__.py
+++ implicit/__init__.py
@@ -1,9 +1,10 @@
 from .als import alternating_least_squares
 
 from . import als
+from . import approximate_als
 from . import bpr
 from . import nearest_neighbours
 
 __version__ = '0.3.6'
 
-__all__ = [alternating_least_squares, als, bpr, nearest_neighbours, __version__]
+__all__ = [alternating_least_squares, als, approximate_als, bpr, nearest_neighbours, __version__]
