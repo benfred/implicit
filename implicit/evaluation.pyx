ec0c0a32b5c1612373eb1a463037d57565d8658f
ita9naiwa <ita9naiwa@gmail.com>
2019-06-08 19:56:26 +0900

    Add new evaluation metric Normalized Discounted Cumulative Sum

--- implicit/evaluation.pyx
+++ implicit/evaluation.pyx
@@ -210,3 +210,87 @@ def mean_average_precision_at_k(model, train_user_items, test_user_items, int K=
 
     progress.close()
     return mean_ap / total
+
+
+@cython.boundscheck(False)
+def ndcg_at_k(model, train_user_items, test_user_items, int K=10,
+              show_progress=True, int num_threads=1):
+    """ Calculates ndcg@K for a given trained model
+
+    Parameters
+    ----------
+    model : RecommenderBase
+        The fitted recommendation model to test
+    train_user_items : csr_matrix
+        Sparse matrix of user by item that contains elements that were used in training the model
+    test_user_items : csr_matrix
+        Sparse matrix of user by item that contains withheld elements to test on
+    K : int
+        Number of items to test on
+    show_progress : bool, optional
+        Whether to show a progress bar
+    num_threads : int, optional
+        The number of threads to use for testing. Specifying 0 means to default
+        to the number of cores on the machine. Note: aside from the ALS and BPR
+        models, setting this to more than 1 will likely hurt performance rather than
+        help.
+
+    Returns
+    -------
+    float
+        the calculated ndcg@k
+    """
+    if not isinstance(train_user_items, csr_matrix):
+        train_user_items = train_user_items.tocsr()
+
+    if not isinstance(test_user_items, csr_matrix):
+        test_user_items = test_user_items.tocsr()
+
+    cdef int users = test_user_items.shape[0], u, i
+    cdef double relevant = 0, total = 0
+    cdef int[:] test_indptr = test_user_items.indptr
+    cdef int[:] test_indices = test_user_items.indices
+
+    cdef int * ids
+    cdef unordered_set[int] * likes
+    cdef double[:] cg = (1.0 / np.log2(np.arange(2, K + 2)))
+
+    progress = tqdm.tqdm(total=users, disable=not show_progress)
+
+    with nogil, parallel(num_threads=num_threads):
+        ids = <int *> malloc(sizeof(int) * K)
+        likes = new unordered_set[int]()
+        try:
+            for u in prange(users, schedule='guided'):
+                # if we don't have any test items, skip this user
+                if test_indptr[u] == test_indptr[u+1]:
+                    with gil:
+                        progress.update(1)
+                    continue
+                memset(ids, 0, sizeof(int) * K)
+
+                with gil:
+                    recs = model.recommend(u, train_user_items, N=K)
+                    for i in range(len(recs)):
+                        ids[i] = recs[i][0]
+                    progress.update(1)
+
+                # mostly we're going to be blocked on the gil here,
+                # so try to do actual scoring without it
+                likes.clear()
+
+                for i in range(test_indptr[u], test_indptr[u+1]):
+                    likes.insert(test_indices[i])
+
+                with gil:
+                    idcg = np.sum(cg[:min(K, likes.size())])
+                    for i in range(K):
+                        if likes.find(ids[i]) != likes.end():
+                            relevant += cg[i] / idcg
+                total += 1
+        finally:
+            free(ids)
+            del likes
+
+    progress.close()
+    return relevant / total
