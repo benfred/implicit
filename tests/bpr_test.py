77c6b0611fb0f26c32e4ec39ff334b512ed41b7a
Hyunsung Iggy Lee <ita9naiwa@gmail.com>
2019-10-24 19:35:22 +0900

    Revert "Merge pull request #289 from chedatomasz/merged_fix"
    This reverts commit 14cd9929f006f002db7ce3a4872b0f24ff9c21f7.

--- tests/bpr_test.py
+++ tests/bpr_test.py
@@ -1,7 +1,5 @@
 import unittest
 
-from scipy.sparse import csr_matrix
-
 from implicit.bpr import BayesianPersonalizedRanking
 from implicit.cuda import HAS_CUDA
 
@@ -13,22 +11,13 @@ class BPRTest(unittest.TestCase, TestRecommenderBaseMixin):
     def _get_model(self):
         return BayesianPersonalizedRanking(factors=3, regularization=0, use_gpu=False)
 
-    # Test issue #264 causing crashes on empty matrices
-    def test_fit_empty_matrix(self):
-        raw = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        return BayesianPersonalizedRanking().fit(csr_matrix(raw), show_progress=False)
-
-    # Test issue #264 causing crashes on almost empty matrices
-    def test_fit_almost_empty_matrix(self):
-        raw = [[0, 0, 0], [0, 1, 0], [0, 0, 0]]
-        return BayesianPersonalizedRanking().fit(csr_matrix(raw), show_progress=False)
-
 
 if HAS_CUDA:
     class BPRGPUTest(unittest.TestCase, TestRecommenderBaseMixin):
 
         def _get_model(self):
             return BayesianPersonalizedRanking(factors=3, regularization=0, use_gpu=True)
+
 
 if __name__ == "__main__":
     unittest.main()
