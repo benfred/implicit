17196a355cc133859d5cb4a5f04acb5ed4e1cd47
tgsmith61591 <tgsmith61591@gmail.com>
2020-04-17 08:50:15 -0500

    Back out rng_utils

--- implicit/bpr.pyx
+++ implicit/bpr.pyx
@@ -11,11 +11,14 @@ from libcpp cimport bool
 from libcpp.algorithm cimport binary_search
 
 import numpy as np
+cimport numpy as np
 import scipy.sparse
+
+import random
+from libcpp.vector cimport vector
 
 import implicit.cuda
 
-from .rng_utils import RNGVector
 from .recommender_base import MatrixFactorizationBase
 from .utils import check_random_state
 
@@ -26,6 +29,34 @@ log = logging.getLogger("implicit")
 # build to fail)
 cdef extern from "bpr.h" namespace "implicit" nogil:
     cdef int get_thread_num()
+
+
+cdef extern from "<random>" namespace "std":
+    cdef cppclass mt19937:
+        mt19937(unsigned int)
+
+    cdef cppclass uniform_int_distribution[T]:
+        uniform_int_distribution(T, T)
+        T operator()(mt19937) nogil
+
+
+cdef class RNGVector(object):
+    """ This class creates one c++ rng object per thread, and enables us to randomly sample
+    liked/disliked items here in a thread safe manner """
+    cdef vector[mt19937] rng
+    cdef vector[uniform_int_distribution[long]] dist
+
+    def __init__(self, int num_threads, long rows, long[:] rng_seeds):
+        if len(rng_seeds) != num_threads:
+            raise ValueError("length of RNG seeds must be equal to num_threads")
+
+        cdef int i
+        for i in range(num_threads):
+            self.rng.push_back(mt19937(rng_seeds[i]))
+            self.dist.push_back(uniform_int_distribution[long](0, rows))
+
+    cdef inline long generate(self, int thread_id) nogil:
+        return self.dist[thread_id](self.rng[thread_id])
 
 
 @cython.boundscheck(False)
