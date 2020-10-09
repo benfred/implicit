26623ee1edcf74cfa61074ba7d89d18224bb494c
ita9naiwa <ita9naiwa@gmail.com>
2020-10-09 18:26:05 +0000

    .

--- tests/cml_test.py
+++ tests/cml_test.py
@@ -7,11 +7,11 @@ from implicit.cml import CollaborativeMetricLearning
 from .recommender_base_test import TestRecommenderBaseMixin
 
 
-class LMFTest(unittest.TestCase, TestRecommenderBaseMixin):
-    def _get_model(self):
-        return CollaborativeMetricLearning(factors=10, regularization=1.0,
+class CMLTest(unittest.TestCase, TestRecommenderBaseMixin):
+    def _get_model(self, factors=8):
+        return CollaborativeMetricLearning(factors=3,
                                            use_gpu=False,
-                                           random_state=43)
+                                           random_state=33)
 
     def test_fit_empty_matrix(self):
         raw = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
