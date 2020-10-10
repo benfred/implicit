14566ae2a31acd2302759f28802123d1d2ca79ea
Mark Douthwaite <mark@douthwaite.io>
2020-10-10 19:07:32 +0100

    Added 'recommender_split' function to 'evaluation' module.

--- implicit/evaluation.pyx
+++ implicit/evaluation.pyx
@@ -3,7 +3,6 @@
 
 import cython
 import numpy as np
-from cython.operator import dereference
 from cython.parallel import parallel, prange
 from scipy.sparse import coo_matrix, csr_matrix
 from tqdm.auto import tqdm
@@ -51,6 +50,83 @@ def train_test_split(ratings, train_percentage=0.8, random_state=None):
     test.eliminate_zeros()
 
     return train, test
+
+
+def _choose(rng, int n, float frac):
+    size = max(1, int(n * frac))
+    return rng.choice(n, size=size, replace=False)
+
+
+def _single_sample(arr):
+    idx = arr.argsort()
+    bins = np.bincount(arr)
+    rand = np.random.rand(bins.sum()) + np.repeat(np.arange(len(bins)), bins)
+    offset = np.r_[0, bins[: -1].cumsum()]
+    return idx[rand.argsort()[offset]]
+
+
+def recommender_split(
+    mat, int n_samples = 1, float train_only_size=0.0, random_state=None
+):
+
+    random_state = check_random_state(random_state)
+
+    if n_samples < 1:
+        raise ValueError("The 'n_samples' must be >= 1.")
+
+    users = mat.row  # user ids, assumes sorted (!)
+    items = mat.col  # item ids
+    data = mat.data
+
+    unique_users, counts = np.unique(users, return_counts=True)
+
+    # get only users with n + 1 interactions
+    candidate_mask = counts > n_samples + 1
+
+    # keep a given subset of users _only_ in the training set.
+    if train_only_size > 0.0:
+        train_only_mask = ~np.isin(
+            unique_users, _choose(random_state, len(unique_users), train_only_size)
+        )
+        candidate_mask = train_only_mask & candidate_mask
+
+    # get unique users who appear in the test set
+    unique_candidate_users = unique_users[candidate_mask]
+    full_candidate_mask = np.isin(users, unique_candidate_users)
+
+    # get all users, items and ratings that match specified requirements to be
+    # included in test set.
+    candidate_users = users[full_candidate_mask]
+    candidate_items = items[full_candidate_mask]
+    candidate_data = data[full_candidate_mask]
+
+    if n_samples == 1:
+        # sample a single item for each candidate user
+        # (i.e. get index of each sampled item)
+        test_idx = _single_sample(candidate_users)
+    else:
+        # todo: can this be vectorized too?
+        test_idx = np.ravel(
+            [random_state.choice(np.where(candidate_users==user)[0], n_samples) for user in unique_candidate_users]
+        )
+
+    # get all remaining remaining candidate user-item pairs, and prepare to append to training set.
+    train_idx = np.setdiff1d(np.arange(len(candidate_users), dtype=int), test_idx)
+
+    # build test matrix
+    test_users = candidate_users[test_idx]
+    test_items = candidate_items[test_idx]
+    test_data = candidate_data[test_idx]
+    test_mat = coo_matrix((test_data, (test_users, test_items)), shape=mat.shape)
+
+    # build training matrix
+    train_users = np.r_[users[~full_candidate_mask], candidate_users[train_idx]]
+    train_items = np.r_[items[~full_candidate_mask], candidate_items[train_idx]]
+    train_data = np.r_[data[~full_candidate_mask], candidate_data[train_idx]]
+    train_mat = coo_matrix((train_data, (train_users, train_items)), shape=mat.shape)
+
+    return train_mat, test_mat
+
 
 
 @cython.boundscheck(False)
--- tests/evaluation_test.py
+++ tests/evaluation_test.py
@@ -3,14 +3,19 @@ from __future__ import print_function
 import unittest
 
 import numpy as np
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, random
 
-from implicit.evaluation import train_test_split
+from implicit.evaluation import recommender_split, train_test_split
 
 
 class EvaluationTest(unittest.TestCase):
     def _get_sample_matrix(self):
         return csr_matrix((np.random.random((10, 10)) > 0.5).astype(np.float64))
+
+    def _get_implicit_matrix(self):
+        m = random(1000, 500, density=0.1, format="csr")
+        m[m > 0] = 1
+        return m.tocoo()
 
     def test_split(self):
         seed = np.random.randint(1000)
@@ -18,6 +23,11 @@ class EvaluationTest(unittest.TestCase):
         train, test = train_test_split(mat, 0.8, seed)
         train2, test2 = train_test_split(mat, 0.8, seed)
         self.assertTrue(np.all(train.todense() == train2.todense()))
+
+    def test_recommender_split_single_sample(self):
+        mat = self._get_implicit_matrix()
+        train, test = recommender_split(mat, n_samples=1)
+        self.assertTrue(((train + test) - mat).nnz == 0)
 
 
 if __name__ == "__main__":
