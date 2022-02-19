e57e73c68cadd5394509212e00b4f9202d120ba0
Ben Frederickson <github@benfrederickson.com>
2022-02-19 10:09:14 -0800

    try again with tests

--- ci/install_cuda.sh
+++ ci/install_cuda.sh
@@ -1,9 +1,11 @@
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
-yum install --setopt=obsoletes=0 -y \
-    cuda-nvcc-11-2 \
-    cuda-cudart-devel-11-2 \
-    libcurand-devel-11-2 \
-    libcublas-devel-11-2 \
-    cuda-nvprof-11-2 \
-    ninja-build
-ln -s cuda-11.2 /usr/local/cuda
+#yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
+#yum install --setopt=obsoletes=0 -y \
+#    cuda-nvcc-11-2 \
+#    cuda-cudart-devel-11-2 \
+#    libcurand-devel-11-2 \
+#    libcublas-devel-11-2 \
+#    cuda-nvprof-11-2 \
+#    ninja-build
+#ln -s cuda-11.2 /usr/local/cuda
+
+yum install -y ninja-build
