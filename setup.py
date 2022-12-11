871e0c7229b012108131b6211cd617e23a3b24bf
Ben Frederickson <github@benfrederickson.com>
2022-12-10 16:17:11 -0800

    Bump version: 0.6.1 → 0.6.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ def exclude_non_implicit_cmake_files(cmake_manifest):
 
 setup(
     name="implicit",
-    version="0.6.1",
+    version="0.6.2",
     description="Collaborative Filtering for Implicit Feedback Datasets",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
