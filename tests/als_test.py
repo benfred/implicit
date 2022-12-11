bf636fab9a1efa434ebb7499a8d21cbe6b91ba54
iggy.ll <iggy.ll@kakacorp.com>
2022-12-11 16:07:35 +0900

    isort

--- tests/als_test.py
+++ tests/als_test.py
@@ -3,13 +3,13 @@ import unittest
 
 import numpy as np
 import pytest
+from implcit.evaluation import ranking_metrics_at_k, train_test_split
 from numpy.testing import assert_array_equal
 from recommender_base_test import RecommenderBaseTestMixin, get_checker_board
 from scipy.sparse import coo_matrix, csr_matrix, random
 
 from implicit.als import AlternatingLeastSquares
 from implicit.gpu import HAS_CUDA
-from implcit.evaluation import ranking_metrics_at_k, train_test_split
 
 # pylint: disable=consider-using-f-string
 
