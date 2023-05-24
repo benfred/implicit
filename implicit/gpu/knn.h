3cb45326893cef22dd6277e3dde41c454eb9ee1c
Ben Frederickson <github@benfrederickson.com>
2023-05-23 17:04:25 -0700

    use RAFT for select_k on the gpu (#656)
    This changes to use RAFT https://github.com/rapidsai/raft for GPU
    top-k code instead of faiss. The RAFT version is quite a bit faster,
    and also doesn't have the same performance issues with small batch
    sizes that faiss has (meaning we can delete a bunch of code that
    was trying to work around that).
    
    RAFT also doesn't have limitations on the size of K, where faiss is limited to k less 
    than 2048.

--- implicit/gpu/knn.h
+++ implicit/gpu/knn.h
@@ -2,11 +2,10 @@
 #define IMPLICIT_GPU_KNN_H_
 #include <memory>
 
+#include <raft/core/resources.hpp>
 #include <rmm/mr/device/device_memory_resource.hpp>
 
 #include "implicit/gpu/matrix.h"
-
-struct cublasContext;
 
 namespace implicit {
 namespace gpu {
@@ -14,19 +13,18 @@ class KnnQuery {
 public:
   KnnQuery(size_t temp_memory = 0);
   ~KnnQuery();
-  cublasContext *blas_handle;
 
   void topk(const Matrix &items, const Matrix &query, int k, int *indices,
             float *distances, float *item_norms = NULL,
             const COOMatrix *query_filter = NULL,
             Vector<int> *item_filter = NULL);
 
-  void argpartition(const Matrix &items, int k, int *indices, float *distances,
-                    bool allow_tiling);
-  void argsort(const Matrix &items, int *indices, float *distances);
+  void argsort(const int *input_indices, const float *input_distances, int rows,
+               int cols, int *indices, float *distances);
 
 protected:
   std::unique_ptr<rmm::mr::device_memory_resource> mr;
+  raft::resources handle;
   size_t max_temp_memory;
 };
 } // namespace gpu
