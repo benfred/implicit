2268ac78d08e4ab94ce53bd48b08bcd012878559
Ben Frederickson <ben@benfrederickson.com>
2023-06-12 21:45:17 -0700

    Bump version: 0.6.2 → 0.7.0

--- implicit/__init__.py
+++ implicit/__init__.py
@@ -1,5 +1,5 @@
 from . import als, approximate_als, bpr, lmf, nearest_neighbours
 
-__version__ = "0.6.2"
+__version__ = "0.7.0"
 
 __all__ = ["als", "approximate_als", "bpr", "nearest_neighbours", "lmf", "__version__"]
