547a3333eb830919ecb74141a10dc0fc0e1cba2f
Ben Frederickson <ben@benfrederickson.com>
2023-11-21 11:20:41 -0800

    see if this builds with cuda 11.4

--- ci/install_cuda.sh
+++ ci/install_cuda.sh
@@ -1,9 +1,9 @@
 yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
 yum install --setopt=obsoletes=0 -y \
-    cuda-nvcc-11-8 \
-    cuda-cudart-devel-11-8 \
-    libcurand-devel-11-8 \
-    libcublas-devel-11-8 \
-    cuda-nvprof-11-8 \
+    cuda-nvcc-11-4 \
+    cuda-cudart-devel-11-4 \
+    libcurand-devel-11-4 \
+    libcublas-devel-11-4 \
+    cuda-nvprof-11-4 \
     ninja-build
-ln -s cuda-11.8 /usr/local/cuda
+ln -s cuda-11.4 /usr/local/cuda
