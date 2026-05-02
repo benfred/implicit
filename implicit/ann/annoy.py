035581ad39b1165f313453fb7de38a02ce4dda3e
Ben Frederickson <github@benfrederickson.com>
2026-05-02 11:06:58 -0700

    Use ruff instead of black/isort/flake8/pylint (#749)

--- implicit/ann/annoy.py
+++ implicit/ann/annoy.py
@@ -124,9 +124,7 @@ class AnnoyModel(RecommenderBase):
 
         # support recalculate_item if possible. TODO: refactor this
         if hasattr(self.model, "_item_factor"):
-            factor = self.model._item_factor(
-                itemid, item_users, recalculate_item
-            )  # pylint: disable=protected-access
+            factor = self.model._item_factor(itemid, item_users, recalculate_item)  # pylint: disable=protected-access
         elif recalculate_item:
             raise NotImplementedError(f"recalculate_item isn't supported with {self.model}")
         else:
@@ -191,9 +189,7 @@ class AnnoyModel(RecommenderBase):
 
         # support recalculate_user if possible (TODO: come back to this since its a bit of a hack)
         if hasattr(self.model, "+_user_factor"):
-            user = self.model._user_factor(
-                userid, user_items, recalculate_user
-            )  # pylint: disable=protected-access
+            user = self.model._user_factor(userid, user_items, recalculate_user)  # pylint: disable=protected-access
         elif recalculate_user:
             raise NotImplementedError(f"recalculate_user isn't supported with {self.model}")
         else:
