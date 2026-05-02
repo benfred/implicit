fc1116f669eb0292c52b5bd52cc2fb87955fecc7
Ben Frederickson <github@benfrederickson.com>
2026-05-02 10:17:53 -0700

    Use ruff instead of black/isort/flake8/pylint

--- implicit/ann/nmslib.py
+++ implicit/ann/nmslib.py
@@ -128,9 +128,7 @@ class NMSLibModel(RecommenderBase):
 
         # support recalculate_item if possible. TODO: refactor this
         if hasattr(self.model, "_item_factor"):
-            factors = self.model._item_factor(
-                itemid, item_users, recalculate_item
-            )  # pylint: disable=protected-access
+            factors = self.model._item_factor(itemid, item_users, recalculate_item)  # pylint: disable=protected-access
         elif recalculate_item:
             raise NotImplementedError(f"recalculate_item isn't supported with {self.model}")
         else:
@@ -199,9 +197,7 @@ class NMSLibModel(RecommenderBase):
 
         # support recalculate_user if possible (TODO: come back to this since its a bit of a hack)
         if hasattr(self.model, "+_user_factor"):
-            user = self.model._user_factor(
-                userid, user_items, recalculate_user
-            )  # pylint: disable=protected-access
+            user = self.model._user_factor(userid, user_items, recalculate_user)  # pylint: disable=protected-access
         elif recalculate_user:
             raise NotImplementedError(f"recalculate_user isn't supported with {self.model}")
         else:
