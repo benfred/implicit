48e50fd15321823198c306da5f17d59f514ce63b
Ben Frederickson <ben@benfrederickson.com>
2026-05-06 20:56:50 -0700

    Add RMM as a dependency of the GPU code

--- implicit/gpu/__init__.py
+++ implicit/gpu/__init__.py
@@ -1,7 +1,16 @@
 from __future__ import absolute_import
 
+import warnings
+
 HAS_CUDA = False
+HAS_RMM = False
+
 try:
+    # RMM is required to enable GPU support - use with 'pip install rmm-cu13'
+    import rmm  # noqa
+
+    HAS_RMM = True
+
     from ._cuda import *  # noqa
 
     get_device_count()  # noqa pylint: disable=undefined-variable
@@ -13,5 +22,8 @@ except RuntimeError as e:
     warnings.warn(
         f"CUDA extension is built, but disabling GPU support because of '{e}'",
     )
-except ImportError:
-    pass
+except ImportError as e:
+    if HAS_RMM:
+        warnings.warn(
+            f"Disabling GPU support because of '{e}'",
+        )
